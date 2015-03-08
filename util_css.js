9ad0d00b20d89f7af0e244445a54be246d17bd60
Andrew Wong <andrew12345@live.com>
2015-03-08 18:44:06 +1100

    * Code cleanup (not really)

--- util_css.js
+++ util_css.js
@@ -1,7 +1,6 @@
 function util_addCSS(css_one, css_two, etc) {
   for (i = 0; i < arguments.length; i++) {
     style = document.createElement('style');
-
     if (style.styleSheet)
       style.styleSheet.cssText = arguments[i];
     else
